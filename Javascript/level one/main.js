4437efe0b959fc96f1b495f8b14f8e343c6840b4
Nikant <nikant20@gmail.com>
2018-08-06 07:39:48 +0530

    Basics if JavaScript

---
+++ Javascript/level one/main.js
@@ -0,0 +1,13 @@
+//while loop demo
+var x = 0;
+
+while (x<5) {
+    console.log("x is currently: "+x);
+    if (x==3) {
+        console.log("x is equal to 3");
+        break;
+        
+    }
+    console.log("x is still less than 5, adding 1 to x");
+    x++;
+}
---
+++ Javascript/level one/myscript.js
@@ -0,0 +1,3 @@
+let pounds = prompt("Enter the weight in Pounds");
+let kg = pounds * 0.454;
+alert("Weight in KGs are: "+kg);
